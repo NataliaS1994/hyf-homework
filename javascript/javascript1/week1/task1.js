acceb9d942bfe0720986d69e59d3f529136991f5
Natalia Sowa <natalia.sowaa@gmail.com>
2021-06-29 17:36:32 +0200

    change future year in javascript1/week1/task1.js

--- javascript/javascript1/week1/task1.js
+++ javascript/javascript1/week1/task1.js
@@ -1,8 +1,7 @@
 console.log("Age-ify!") // This is Age-ify 
 
 let yearOfBirth = 1994;
-let yearFuture = 2045;
-yearFuture = 2027;
+let yearFuture = 2034;
 let age = yearFuture - yearOfBirth;
 
 console.log("You will be " + age + " years old in " + yearFuture)
