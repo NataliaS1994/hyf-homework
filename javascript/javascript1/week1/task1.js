011fa3b3560d28cd4fde9ccd20cc3a41f16c561c
Natalia Sowa <natalia.sowaa@gmail.com>
2021-06-29 17:08:10 +0200

    javascript1/week1/task1.js

---
+++ javascript/javascript1/week1/task1.js
@@ -0,0 +1,8 @@
+console.log("Age-ify!") // This is Age-ify 
+
+let yearOfBirth = 1994;
+let yearFuture = 2045;
+yearFuture = 2027;
+let age = yearFuture - yearOfBirth;
+
+console.log("You will be " + age + " years old in " + yearFuture)
