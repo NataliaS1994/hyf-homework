146eb51160b71ceedf1ef4c992f3e82b20efcbeb
Natalia Sowa <natalia.sowaa@gmail.com>
2021-06-30 22:03:50 +0200

    change houseCost in Peter's calculation

--- javascript/javascript1/week1/task3.js
+++ javascript/javascript1/week1/task3.js
@@ -4,7 +4,7 @@ var houseWidth = 8
 var houseDepth = 10
 var houseHight = 10
 var gardenSizeInM2 = 100
-var houseCost = 100
+var houseCost = 2500000
 let volumeInMeters = houseWidth * houseDepth * houseHight;
 let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 if (housePrice < houseCost) {
