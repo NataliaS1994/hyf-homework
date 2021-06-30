a1fb726f0d19be0ade4af981938d3891d4214cc9
Natalia Sowa <natalia.sowaa@gmail.com>
2021-06-30 21:55:48 +0200

    add changes to Peter's house calculation

--- javascript/javascript1/week1/task3.js
+++ javascript/javascript1/week1/task3.js
@@ -1,33 +1,15 @@
 console.log('Housey pricey!') // Housey pricey 
-
 //Peter's calculation
-var houseWidth: 8,
-var houseDepth: 10,
-var houseHight: 10,
-var gardenSizeInM2: 100,
-var houseCost: 2500000
-
+var houseWidth = 8
+var houseDepth = 10
+var houseHight = 10
+var gardenSizeInM2 = 100
+var houseCost = 100
 let volumeInMeters = houseWidth * houseDepth * houseHight;
 let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
-
 if (housePrice < houseCost) {
-    console.log('Peter pays more than the actual house price')
-} else {
-    console.log('Peter pays less than the actual house price')
+    console.log ("Peter pays more than the value of the house")   
 }
-
-//Julia's calculation
-var houseWidth: 5,
-var houseDepth: 11,
-var houseHight: 8,
-var gardenSizeInM2: 70,
-var houseCost: 2500000
-
-let volumeInMeters = houseWidth * houseDepth * houseHight;
-let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
-
-if (housePrice < houseCost) {
-    console.log('Julia pays more than the actual house price')
-} else {
-    console.log('Julia pays less than the actual house price')
+else {
+    console.log("Peter pays less than the value of the house")
 }
