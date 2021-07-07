d9758dbe275424f3bea92f49c89f8057a58063b4
Natalia Sowa <natalia.sowaa@gmail.com>
2021-07-07 22:43:10 +0200

    add gender to flight booking

--- javascript/javascript1/week2/flight-booking.js
+++ javascript/javascript1/week2/flight-booking.js
@@ -16,6 +16,29 @@ function getFullName (firstName, surName) {
       } else 
         console.log ("login-failed")
     }
+
+    //What if the person is a woman?
+
+function getFullName (firstName, surName, useFormalName, gender) {
+    if (gender === "woman" && useFormalName === true) {
+      console.log ("Lady" + " " + firstName + " " + surName);
+    } else if (gender === "woman" && useFormalName === false) {
+      console.log (firstName + surName);
+    } else if ( gender === "man" && useFormalName === true) {
+      console.log ("Lord" + firstName + " " + surName);
+    } else if (gender === "man" && useFormalName === false) {
+      console.log (firstName + surName);
+    } else
+      console.log ("login-failed");
+  }
+  
+  getFullName ("Natalia", "Sowa", true, "woman"); // returns "Lady Natalia Sowa"
+  getFullName ("Natalia", "Sowa", false, "woman"); // returns "Natalia Sowa"
+  getFullName ("Benjamin", "Hughes", true, "man"); // returns "Lord Benjamin Hughes"
+  getFullName ("Benjamin", "Hughes", false, "man"); // returns "Benjamin Hughes"
+  getFullName ("Benjamin", "Hughes", false, "x"); // returns "login-failed"
+  
+  
   
   getFullName ("Benjamin", "Hughes", true); //returns "Lord Benjamin Hughes"`
   getFullName ("Benjamin", "Hughes", false); // returns "Benjamin Hughes"
