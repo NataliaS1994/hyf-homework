848798d39d5ffdc2237275f75f2c4250e2136245
Natalia Sowa <natalia.sowaa@gmail.com>
2021-07-26 17:29:30 +0200

    add travelTime.js

---
+++ javascript/javascript1/week3/travelTime.js
@@ -0,0 +1,21 @@
+ //When will we be there??
+
+const travelInformation = {
+    speed: 50,
+    destinationDistance: 432,
+};
+
+function getArrivalTime() {
+    const approxTime = travelInformation.destinationDistance / travelInformation.speed;
+    let hours = Math.floor(approxTime);
+    let minutes = Math.floor((approxTime % 1) * 60);
+
+    return `Travel time is ${hours} hours and ${minutes} minutes.`
+}
+
+const travelTime = getArrivalTime(travelInformation);
+
+console.log((travelTime)); // 8 hours and 38 minutes
+
+
+getArrivalTime();
