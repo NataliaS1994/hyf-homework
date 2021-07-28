72f14fc5b56f574ba0d29c6f046044ee989fea22
Natalia Sowa <natalia.sowaa@gmail.com>
2021-07-28 23:11:50 +0200

    add saveNote.js(3)

--- javascript/javascript1/week3/saveNote.js
+++ javascript/javascript1/week3/saveNote.js
@@ -30,3 +30,17 @@ function getNote(id) {
 const firstNote = getNote(1);
 const checkError = getNote(4);
 console.log(firstNote); // {content: 'Pick up groceries', id: 1}
+
+//Log out notes
+
+function logOutNotesFormatted() {
+	for (let i = 0; i < notes.length; i++) {
+		console.log(`The note with id: ${notes[i].id}, has the following note text: ${notes[i].content}`)
+	}
+}
+
+
+logOutNotesFormatted(); // should log out the text below
+
+// The note with id: 1, has the following note text: Pick up groceries
+// The note with id: 2, has the following note text: Do laundry
