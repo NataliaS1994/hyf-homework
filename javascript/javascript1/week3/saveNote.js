2a4cbd7106e12e554ba9ed4649a1d181deb8e93f
Natalia Sowa <natalia.sowaa@gmail.com>
2021-07-28 23:07:01 +0200

    add saveNote.js(2)

--- javascript/javascript1/week3/saveNote.js
+++ javascript/javascript1/week3/saveNote.js
@@ -1,14 +1,32 @@
 //Save a note
 
-const notes = [];
+let notes = [];
 
 function saveNote(content, id) {
-  if (typeof content ==='string' && id ==='number') {
-    notes.push({ content, id });
+	if (typeof content === 'string' && typeof id === 'number') {
+		notes.push({
+			content,
+			id
+		});
+	}
 }
-  }
 
 saveNote("Pick up groceries", 1);
 saveNote("Do laundry", 2);
 
 console.log(notes); // [{content: 'Pick up groceries', id: 1}, {content: 'Do laundry', id: 2}]
+
+
+//Get a note
+function getNote(id) {
+	for (let i = 0; i < notes.length; i++) {
+		if (typeof id === 'number' && id === notes[i].id) {
+			return notes[i];
+		} else {
+			return console.log('Error');
+		}
+	}
+}
+const firstNote = getNote(1);
+const checkError = getNote(4);
+console.log(firstNote); // {content: 'Pick up groceries', id: 1}
