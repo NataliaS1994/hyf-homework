77fe444138febfd50cd405279b378a34a101e1f1
Natalia Sowa <natalia.sowaa@gmail.com>
2021-08-11 23:51:20 +0200

    homework fixed

--- javascript/javascript1/week4/voiceAssistant.js
+++ javascript/javascript1/week4/voiceAssistant.js
@@ -1,99 +1,102 @@
-const userName = []
-const todoList = []
-const computation = ['+', '-', '/', '*']
+let userName = []
+let toDo = []
 
-//Create a function called getReply(command)
 function getReply(command) {
-	if (command.indexOf('name is') !== -1) {
-		userName.push(command.slice(command.indexOf('name is') + 'name is'.length))
+
+	//Hello my name is Benjamin
+	if (command.includes("Hello my name is")) {
+		userName = command.split(' ').splice(-1);
 		return `Nice to meet you ${userName[0]}`
+
 	}
 
-	if (command.indexOf('is my name') !== -1) {
-		return `Your name is ${userName[0]}`
+	//whats my name?
+	if (command.includes("What is my name")) {
+		if (userName !== "") {
+			return `Your name is ${userName[0]}`
+
+		} else {
+			return "I didn't hear your name";
+		}
+	}
+	//add activities
+	if (command.includes("Add")) {
+		let todoItem = command.split('Add').pop().split('to my todo').splice(0, 1);
+		todoItem = todoItem.toString().trim();
+		toDo.push(todoItem);
+		return `${todoItem} added to your todo`;
 	}
 
-	if (command.toLowerCase().indexOf('add') !== -1) {
-		todoList.push(
-			command.slice(command.indexOf('add') + 5, command.indexOf('to') - 1)
-		)
-	}
+	//remove activity such as f.ex fishing
+	if (command.includes("Remove")) {
+		let todoItem = command.split('Remove').pop().split('from my todo').splice(0, 1);
+		todoItem = todoItem.toString().trim();
+		const index = toDo.indexOf(todoItem);
+		if (index > -1) {
+			toDo.splice(index, 1);
+			return `Removed ${todoItem} from your todo`
 
-	for (let i = 0; i < todoList.length; i++) {
-		if (
-			command.includes(todoList[i]) &&
-			command.toLowerCase().indexOf('add') !== -1
-		) {
-			return `${todoList[i]} added to your todo`
 		}
 	}
 
-	if (command.toLowerCase().includes('remove')) {
-		for (let i = 0; i < todoList.length; i++) {
-			if (command.includes(todoList[i])) {
-				let removedItem = todoList.splice(
-					todoList.indexOf(todoList[i]),
-					1
-				)
-				return `Removed ${removedItem} from your todo`
-			}
+	//return activities
+	if (command.includes("on my todo?")) {
+
+		if (toDo.length > 1) {
+			let noOfItems = toDo.length;
+			let lastItem = toDo.pop(); //Last item removed 
+			return `You have ${noOfItems} activities ${toDo} and ${lastItem} `;
+		}
+
+	}
+	//demand the date
+	if (command.includes("today")) {
+		var today = new Date();
+		let currentMonth = today.toLocaleString('default', {
+			month: 'long'
+		});
+		return today.getDate() + '. of ' + currentMonth + ' ' + today.getFullYear();
+	}
+
+	//math calculations
+	if (command.includes("What is")) {
+		const mathArray = command.match(/(\d+)\s*([\+\-\*\/\%])\s*(\d+)/);
+		switch (mathArray[2]) {
+			case '+':
+				return parseInt(mathArray[1]) + parseInt(mathArray[3]);
+			case '-':
+				return parseInt(mathArray[1]) - parseInt(mathArray[3]);
+			case '*':
+				return parseInt(mathArray[1]) * parseInt(mathArray[3]);
+			case '/':
+				return parseInt(mathArray[1]) / parseInt(mathArray[3]);
+			case '%':
+				return parseInt(mathArray[1]) % parseInt(mathArray[3]);
 		}
 	}
 
-	if (command.toLowerCase().includes('What is on my todo')) {
-		return `You have ${todoList} in your todo list`
+	//Timer set for 4 min
+	if (command.includes("Set a timer")) {
+
+		let commandArray = command.split(' ');
+		let minutes = commandArray[commandArray.length - 2];
+		let milliSeconds = minutes * 60 * 1000; //milliseconds
+		setTimeout(function () {
+			console.log("Timer completed");
+		}, milliSeconds);
+		return `Timer set for ${minutes} minutes`;
 	}
 
-	if (command.includes('today')) {
-		return `Today is the ${new Date().getDate()}th of ${new Date().toLocaleString(
-            'en-us',
-            { month: 'long', year: 'numeric' }
-        )}.`
-	}
-
-	for (let index = 0; index < computation.length; index++) {
-		if (command.includes(computation[index])) {
-			let numbers = command.match(/\d+/g)
-			if (computation[index] === '+') {
-				let result = parseInt(numbers[0]) + parseInt(numbers[1])
-				return result
-			}
-			if (computation[index] === '/') {
-				return `${numbers[0] / numbers[1]}`
-			}
-			if (computation[index] === '*') {
-				return `${numbers[0] * numbers[1]}`
-			}
-			if (computation[index] === '-') {
-				return `${numbers[0] - numbers[1]}`
-			}
-		}
-	}
-
-	if (command.toLowerCase().includes('minutes')) {
-		const totalMinutes = command.match(/\d+/)
-		const totalInMilliseconds = totalMinutes[0] * 60 * 1000
-		console.log(`Timer set to ${totalMinutes[0]} minutes.`)
-
-		function setTimer(minutes) {
-			console.log(minutes + ' minut(s) are done')
-		}
-
-		setTimeout(setTimer, totalInMilliseconds, totalMinutes)
-
-	} else {
-		return `Goodbye`
-	}
 }
 
-console.log(getReply('Hello my name is Benjamin'))
-console.log(getReply('What is my name?'))
-console.log(getReply('Add fishing to my todo'))
-console.log(getReply('Add singing in the shower to my todo'))
-console.log(getReply('Remove fishing from my todo'))
-console.log(getReply('What is on my todo?'))
-console.log(getReply('What day is it today?'))
-console.log(getReply('what is 33 + 3'))
-console.log(getReply('what is 4 * 12'))
-console.log(getReply('Set timer to 2 minutes'))
-console.log(getReply(''))
+console.log(getReply("Hello my name is Benjamin"));
+console.log(getReply(""));
+console.log(getReply("What is my name"));
+console.log(getReply("Add fishing to my todo"));
+console.log(getReply("Add singing in the shower room to my todo"));
+console.log(getReply("Remove fishing from my todo"));
+console.log(getReply("Add dancing to my todo"));
+console.log(getReply("What is on my todo?"));
+console.log(getReply("What day is it today?"));
+console.log(getReply("What is 4*12"));
+console.log(getReply("Set a timer for 4 minutes"));
