b6ff5a2d0076bdadc837bcc8edd8def6d125ec75
Natalia Sowa <natalia.sowaa@gmail.com>
2021-08-24 23:04:00 +0200

    add task 4

--- javascript/javascript2/week2/movies.js
+++ javascript/javascript2/week2/movies.js
@@ -7,3 +7,14 @@ const longTitleMovies = movies.filter(movie => movie.title.length >= 30);
 //Count the number of movies made between 1980-1989 (including both the years)
 const moviesFromTheEighties = movies.filter (movie => (movie.year > 1980 && movie.year <= 1989).length);
 console.log(moviesFromTheEighties);
+
+//Create a new array that has an extra key called tag. The tag is based on the rating: Good (>= 7), Average (>= 4 and < 7), Bad (< 4)
+const moviesWithTag = movies.map(movie => {
+    if(movie.rating >= 7) {
+      movie.tag = "Good"
+    } else if(movie.rating >= 4) {
+      movie.tag = "Average"
+    } else {
+      movie.tag = "Bad"
+  })
+  console.log(moviesWithTag);
