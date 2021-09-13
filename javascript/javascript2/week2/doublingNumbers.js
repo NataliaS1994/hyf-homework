90206f4257daeecd88332d9ac4a4d081b71b07d2
Natalia Sowa <natalia.sowaa@gmail.com>
2021-09-13 21:12:47 +0200

    changing let to const

--- javascript/javascript2/week2/doublingNumbers.js
+++ javascript/javascript2/week2/doublingNumbers.js
@@ -1,4 +1,4 @@
-let numbers = [1, 2, 3, 4];
-let newNumbers = numbers.filter(number => number % 2 !== 0).map(number => number * 2);
+const numbers = [1, 2, 3, 4];
+const newNumbers = numbers.filter(number => number % 2 !== 0).map(number => number * 2);
 
 console.log("The doubled numbers are", newNumbers); // [2, 6] 
