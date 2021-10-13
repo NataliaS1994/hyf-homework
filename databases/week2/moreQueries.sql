726dfcc63f85b67f4c9179ed17be660c669dd56e
Natalia Sowa <natalia.sowaa@gmail.com>
2021-10-13 21:13:48 +0200

    feedback implementation

--- databases/week2/moreQueries.sql
+++ databases/week2/moreQueries.sql
@@ -16,11 +16,10 @@ WHERE user.name = 'Donald Duck' AND status.name='Not started';
 
 
 -- Get all the tasks for 'Maryrose Meadows' that were created in september (hint: month(created)=month_number)
-SELECT task.title, user.name, task.created
+SELECT title, month(created), user.name Created_by
 FROM task
-JOIN user_task ON task.id = user_task.task_id
-JOIN user ON user.id = user_task.user_id
-WHERE user.name = 'Maryrose Meadows' AND month(created)= 9;
+JOIN user ON task.user_id=user.id
+WHERE user.name = 'Maryrose Meadows' AND month(created)=(09);
 
 -- Find how many tasks where created in each month, e.g. how many tasks were created in october, how many tasks were created in november, etc. (hint: use group by)
 SELECT COUNT(task.id) AS "Task`s_count", month(task.created)
