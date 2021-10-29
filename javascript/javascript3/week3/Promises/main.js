2ae5a84f15a873f18e175d2e44a7ac728bb0656a
Natalia Sowa <natalia.sowaa@gmail.com>
2021-10-29 13:28:34 +0200

    add changes to the exercise with Promises

--- javascript/javascript3/week3/Promises/main.js
+++ javascript/javascript3/week3/Promises/main.js
@@ -1,27 +1,41 @@
-//fetching data using API
-let userName1 = fetch(
-    "https://api.github.com/search/repositories?q=user:Divyajg"
-);
-let userName2 = fetch(
-    "https://api.github.com/search/repositories?q=user:sunilkumarmahajan"
-);
-let userName3 = fetch(
-    "https://api.github.com/search/repositories?q=user:hparul"
-);
+const repositoryUl = document.getElementById("repositoryUl");
+const userNames = ['Divyajg', 'NataliaS1994', 'Infamou5']
+const urls = []
+userNames.forEach(userName => urls.push(fetch(`https://api.github.com/search/repositories?q=user:${userName}`)))
 
-Promise.all([userName1, userName2, userName3])
-    .then((responses) => responses.forEach((response) => render(response.json())))
-    .catch((error) => console.log("error"));
+let users = () => {
+  Promise.all(urls)
+    .then((responses) => {
+      return Promise.all(responses.map((response) => response.json()));
+    })
+    .then(function (datas) {
+      console.log(datas);
+      //new array that contains all the item arrays
+      let usersData = datas.map((data) => data.items);
 
-function render(repositoryItem) {
-    repositoryItem.then((data) => {
-        console.log(data);
-        let li = document.createElement("li");
-        let ul = document.getElementById("ul");
-        let p = document.createElement("p");
-        p.innerHTML = `${data.items[0].owner.login}`;
-        li.textContent = `GitHub owner's nickname:${data.items[0].owner.login}, RepositoryUrl:${data.items[0].owner.repos_url} , Url : ${data.items[0].owner.html_url}`;
-        ul.appendChild(p);
-        ul.appendChild(li);
+      usersData.forEach((el) => {
+        const li = document.createElement("li");
+        // separates arrays for each user
+        const repos = el;
+
+        //list with the repositories for all users
+        let usersRepos = "";
+        repos.forEach(
+          (repo) => (usersRepos += `<li><span>${repo.name}</span> : <a href="${repo.html_url}">${repo.html_url}</a></li>`)
+        );
+
+        li.innerHTML = `
+            <li style="font-size:18px;color:green;font-weight:bolder">${repos[0].owner.login}\'s repositories</li>
+            <ul>${usersRepos}</ul>
+            `;
+
+        repositoryUl.append(li);
+      });
+    })
+    .catch(function (error) {
+      // if error then log it
+      console.log(error);
     });
-} 
+};
+
+users();
